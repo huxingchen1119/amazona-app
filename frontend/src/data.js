6db8a7e07e97b9d47cab546f823899d6172b8764
thea <huxingchen1119@gmail.com>
2023-05-08 19:55:35 -0400

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Cos Midi Dress',
+            category:'Dresses',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Cos',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'2',
+            name:'Adidas Fit Shirt',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:100,
+            brand:'Adidas',
+            rating:4.0,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'3',
+            name:'Lemaire Free Shirt',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:120,
+            brand:'Lemaire',
+            rating:5.0,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'4',
+            name:'Lululemon Slim Pant',
+            category:'Pants',
+            image:'/images/p4.jpg',
+            price:120,
+            brand:'Lululemon',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'5',
+            name:'Puma Slim Pant',
+            category:'Pants',
+            image:'/images/p5.jpg',
+            price:65,
+            brand:'Puma',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'6',
+            name:'Adidas Samba',
+            category:'Shoes',
+            image:'/images/p6.jpg',
+            price:120,
+            brand:'Adidas',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+        },
+    ]
+}
+export default data;
